af6ab2b784e03510d332b8531a85a4e12db7c810
Raghul <raghulchandramouli@gmail.com>
2025-04-20 14:21:53 +0530

    new maitainer added

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as f:
+    description = f.read()
 setup(
-    name="pocket_ml",
-    version="0.1.0",
+    name="pocket_ml",  # You might need to change this if the name is taken
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         'numpy>=1.19.0',
@@ -10,33 +13,8 @@ setup(
         'scikit-learn>=0.24.0',
         'matplotlib>=3.3.0',
         'seaborn>=0.11.0'
-    ]
-    # Removed metadata fields now defined in pyproject.toml
-    # author="Raghul",
-    # author_email="your.email@example.com",
-    # description="A simplified machine learning library for easy ML workflows",
-    # long_description=open('README.md').read(),
-    # long_description_content_type="text/markdown",
-    # url="https://github.com/raghulchandramouli/pocket_ml",
-    # project_urls={
-    #     "Documentation": "https://pocket_ml.readthedocs.io",
-    #     "Bug Reports": "https://github.com/raghulchandramouli/pocket_ml/issues",
-    #     "Source Code": "https://github.com/raghulchandramouli/pocket_ml"
-    # },
-    # classifiers=[
-    #     "Development Status :: 3 - Alpha",
-    #     "Intended Audience :: Developers",
-    #     "Intended Audience :: Science/Research",
-    #     "Programming Language :: Python :: 3",
-    #     "Programming Language :: Python :: 3.7",
-    #     "Programming Language :: Python :: 3.8",
-    #     "Programming Language :: Python :: 3.9",
-    #     "License :: OSI Approved :: MIT License",
-    #     "Operating System :: OS Independent",
-    #     "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    #     "Topic :: Software Development :: Libraries :: Python Modules"
-    # ],
-    # keywords="machine learning, data science, classification, visualization",
-    # python_requires='>=3.7',
-    # license="MIT", # Removed to rely on pyproject.toml
+    ],
+    
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
