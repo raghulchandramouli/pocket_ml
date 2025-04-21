4ae18dfa4651102c9d963c7c60ab4ba75b42e772
Raghul <raghulchandramouli@gmail.com>
2025-04-21 18:39:24 +0530

    refactor: move preprocessing and visualization to dedicated modules

--- pocket_ml/__init__.py
+++ pocket_ml/__init__.py
@@ -7,6 +7,6 @@ from .algorithms.classification.classifier import Classifier
 from .preprocessing.data_preprocessor import DataPreprocessor
 from .visualization.visualizer import Visualizer
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 __author__ = 'Raghul'
 __all__ = ['Classifier', 'DataPreprocessor', 'Visualizer']
