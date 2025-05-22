62e9802bbcc91e8888255927b3831100b8cd7c8a
Raghul <raghulchandramouli@gmail.com>
2025-05-22 11:19:27 +0530

    change to release version

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ with open("README.md", "r") as f:
     
 setup(
     name="pocket_ml",  # You might need to change this if the name is taken
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         'numpy>=1.19.0',
         'pandas>=1.2.0',
         'scikit-learn>=0.24.0',
         'matplotlib>=3.3.0',
-        'seaborn>=0.11.0'
+        'seaborn>=0.11.0',
+       'numba>=0.53.0',
     ],
     
     long_description=description,
